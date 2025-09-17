39d693270cbc0c5d7f859b7ee345497ad43cbd3f
mariosinnadamas <mariosaeztellez@gmail.com>
2025-09-17 09:11:26 +0200

    Ejercicios 1.1 completos, sin corregir

---
+++ src/ejercicios11/Ejercicio8.java
@@ -0,0 +1,9 @@
+package ejercicios11;
+
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        String ruta = "CarpetaLeer";
+        Ejercicio8Clase info = new Ejercicio8Clase(ruta);
+        System.out.println(info.showInfo());
+    }
+}
